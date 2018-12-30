c972731a5fb268bb53c73048cda5b7271ba6080e
Julia <julia@Manels-MacBook-Air.local>
2018-12-30 11:49:12 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,16 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import getCounter from './worker';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      counter: 0
+    }
+  }
+
+  updateState = () => {
+    this.setState(state => {
+      return {
+        counter: getCounter()
+      }
+    })
+  }
+
   render() {
     return (
       <div className="App">
+      <button onClick={this.updateState}></button>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
+          {this.state.counter}
           <a
             className="App-link"
             href="https://reactjs.org"
